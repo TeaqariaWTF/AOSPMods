5f83f9903807664d7c3169581df6b92adafafdb9
Siavash <25217714+siavash79@users.noreply.github.com>
2025-05-11 16:20:09 +0330

    changed the way tablet mode is decided

--- app/src/main/java/sh/siava/pixelxpert/utils/DisplayUtils.java
+++ app/src/main/java/sh/siava/pixelxpert/utils/DisplayUtils.java
@@ -1,28 +1,9 @@
 package sh.siava.pixelxpert.utils;
 
-import android.content.Context;
-import android.util.DisplayMetrics;
-import android.view.WindowManager;
-
 import sh.siava.pixelxpert.PixelXpert;
 
 public class DisplayUtils {
-
-    @SuppressWarnings("deprecation")
-    public static boolean isTablet() {
-        DisplayMetrics metrics = new DisplayMetrics();
-
-        WindowManager windowManager = (WindowManager) PixelXpert.get().getSystemService(Context.WINDOW_SERVICE);
-        if (windowManager != null) {
-            windowManager.getDefaultDisplay().getMetrics(metrics);
-
-            float yInches = metrics.heightPixels / metrics.ydpi;
-            float xInches = metrics.widthPixels / metrics.xdpi;
-            double diagonalInches = Math.sqrt(Math.pow(xInches, 2) + Math.pow(yInches, 2));
-
-            return diagonalInches >= 7.0;
-        }
-
-        return false;
-    }
+	public static boolean isTablet() {
+		return PixelXpert.get().getResources().getConfiguration().smallestScreenWidthDp >= 600;
+	}
 }
