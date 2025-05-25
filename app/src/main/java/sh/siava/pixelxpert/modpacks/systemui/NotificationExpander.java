7831f3cdaf11c01c06d042da3a834fe6f33b517f
Siavash <25217714+siavash79@users.noreply.github.com>
2025-05-25 13:39:23 +0330

    CHANGELOG: A16qpr1b1: Fixed notification expand buttons

--- app/src/main/java/sh/siava/pixelxpert/modpacks/systemui/NotificationExpander.java
+++ app/src/main/java/sh/siava/pixelxpert/modpacks/systemui/NotificationExpander.java
@@ -1,20 +1,30 @@
 package sh.siava.pixelxpert.modpacks.systemui;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
 import static de.robv.android.xposed.XposedHelpers.callMethod;
 import static de.robv.android.xposed.XposedHelpers.getObjectField;
 import static sh.siava.pixelxpert.modpacks.XPrefs.Xprefs;
 import static sh.siava.pixelxpert.modpacks.utils.toolkit.ObjectTools.tryParseInt;
+import static sh.siava.pixelxpert.modpacks.utils.toolkit.ReflectionTools.reAddView;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
 import androidx.core.content.res.ResourcesCompat;
 
 import java.util.Collection;
+import java.util.regex.Pattern;
 
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import sh.siava.pixelxpert.R;
@@ -38,8 +48,7 @@ public class NotificationExpander extends XposedModPack {
 	private static int notificationDefaultExpansion = DEFAULT;
 	private Button ExpandBtn, CollapseBtn;
 	private FrameLayout FooterView;
-	private FrameLayout BtnLayout;
-	private static int fh = 0;
+	private LinearLayout BtnLayout;
 	private Object Scroller;
 	private Object NotifCollection = null;
 
@@ -71,16 +80,7 @@ public class NotificationExpander extends XposedModPack {
 		ReflectedClass FooterViewButtonClass = ReflectedClass.of("com.android.systemui.statusbar.notification.row.FooterViewButton");
 		ReflectedClass NotifCollectionClass = ReflectedClass.ofIfPossible("com.android.systemui.statusbar.notification.collection.NotifCollection");
 		ReflectedClass NotificationPanelViewControllerClass = ReflectedClass.of("com.android.systemui.shade.NotificationPanelViewController");
-		ReflectedClass FooterViewClass;
-
-
-		try { //14AP11
-			FooterViewClass = ReflectedClass.of("com.android.systemui.statusbar.notification.footer.ui.view.FooterView");
-		}
-		catch (Throwable ignored) //Older
-		{
-			FooterViewClass = ReflectedClass.of("com.android.systemui.statusbar.notification.row.FooterView");
-		}
+		ReflectedClass FooterViewClass = ReflectedClass.of("com.android.systemui.statusbar.notification.footer.ui.view.FooterView");
 
 		//region default notification state
 		NotificationPanelViewControllerClass
@@ -93,30 +93,51 @@ public class NotificationExpander extends XposedModPack {
 
 		//Notification Footer, where shortcuts should live
 		FooterViewClass
+				.after(Pattern.compile("updateColors.*"))
+				.run(param -> updateFooterBtn());
+
+		FooterViewClass
 				.after("onFinishInflate")
 				.run(param -> {
 					FooterView = (FrameLayout) param.thisObject;
 
-					BtnLayout = new FrameLayout(mContext);
-					FrameLayout.LayoutParams BtnFrameParams = new FrameLayout.LayoutParams(Math.round(fh * 2.5f), fh);
+					FooterView.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
+						@Override
+						public void onViewAttachedToWindow(@NonNull View v) {
+							updateFooterBtn();
+						}
+						@Override
+						public void onViewDetachedFromWindow(@NonNull View v) {
+						}
+					});
+
+					BtnLayout = new LinearLayout(mContext);
+					BtnLayout.setClipChildren(false);
+
+					LinearLayout.LayoutParams BtnFrameParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 					BtnFrameParams.gravity = Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM;
 					BtnLayout.setLayoutParams(BtnFrameParams);
 
+					reAddView(BtnLayout,FooterView.findViewById(idOf("history_button")));
+
 					ExpandBtn = (Button) FooterViewButtonClass.getClazz().getConstructor(Context.class).newInstance(mContext);
+					Drawable expandArrows = ResourcesCompat.getDrawable(ResourceManager.modRes, R.drawable.ic_expand, mContext.getTheme());
+					ExpandBtn.setForeground(expandArrows);
+
 					BtnLayout.addView(ExpandBtn);
 
-					FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(fh, fh);
-					layoutParams.gravity = Gravity.START | Gravity.BOTTOM;
-					ExpandBtn.setLayoutParams(layoutParams);
+					reAddView(BtnLayout, FooterView.findViewById(idOf("dismiss_text")));
 
 					ExpandBtn.setOnClickListener(v -> expandAll(true));
 
 					CollapseBtn = (Button) FooterViewButtonClass.getClazz().getConstructor(Context.class).newInstance(mContext);
+					Drawable collapseArrows = ResourcesCompat.getDrawable(ResourceManager.modRes, R.drawable.ic_collapse, mContext.getTheme());
+					CollapseBtn.setForeground(collapseArrows);
+
+
 					BtnLayout.addView(CollapseBtn);
 
-					FrameLayout.LayoutParams lpc = new FrameLayout.LayoutParams(fh, fh);
-					lpc.gravity = Gravity.END | Gravity.BOTTOM;
-					CollapseBtn.setLayoutParams(lpc);
+					reAddView(BtnLayout,FooterView.findViewById(idOf("settings_button")));
 
 					CollapseBtn.setOnClickListener(v -> expandAll(false));
 
@@ -142,27 +163,56 @@ public class NotificationExpander extends XposedModPack {
 				.run(param -> Scroller = param.thisObject);
 	}
 
+	@SuppressLint("DiscouragedApi")
+	private int idOf(String name) {
+		return mContext.getResources().getIdentifier(name, "id", mContext.getPackageName());
+	}
+
 	private void updateFooterBtn() {
 		if(FooterView == null) return; //Footer not inflated yet
 
-		View mClearAllButton = (View) getObjectField(FooterView, "mClearAllButton"); //A13
+		Resources res = mContext.getResources();
 
-		int fh = mClearAllButton.getLayoutParams().height;
+		Button clearAllButton = BtnLayout.findViewById(idOf("dismiss_text"));
 
-		if (fh > 0) { //sometimes it's zero. we don't need that
-			NotificationExpander.fh = fh;
-		}
-		int tc = (int) callMethod(mClearAllButton, "getCurrentTextColor");
+		LinearLayout.LayoutParams dismissParams = (LinearLayout.LayoutParams) clearAllButton.getLayoutParams();
+		dismissParams.width = 0;
+		dismissParams.weight = 1;
 
-		Drawable expandArrows = ResourcesCompat.getDrawable(ResourceManager.modRes, R.drawable.ic_expand, mContext.getTheme());
-		expandArrows.setTint(tc);
-		ExpandBtn.setBackground(expandArrows);
+		@SuppressLint({"UseCompatLoadingForDrawables", "DiscouragedApi"})
+		Drawable backgroundShape = res.getDrawable(res.getIdentifier("notif_footer_btn_background", "drawable", mContext.getPackageName()), mContext.getTheme());
 
-		Drawable collapseArrows = ResourcesCompat.getDrawable(ResourceManager.modRes, R.drawable.ic_collapse, mContext.getTheme());
-		collapseArrows.setTint(tc);
-		CollapseBtn.setBackground(collapseArrows);
+		ExpandBtn.setBackground(backgroundShape);
+		CollapseBtn.setBackground(backgroundShape);
 
-		BtnLayout.setVisibility(notificationExpandallEnabled ? View.VISIBLE : View.GONE);
+
+		int textColor =  clearAllButton.getCurrentTextColor();
+		ExpandBtn.getForeground().setTint(textColor);
+		CollapseBtn.getForeground().setTint(textColor);
+
+		setLayouParams(ExpandBtn, true);
+		setLayouParams(CollapseBtn, false);
+
+		ExpandBtn.setVisibility(notificationExpandallEnabled ? VISIBLE : GONE);
+		CollapseBtn.setVisibility(ExpandBtn.getVisibility());
+	}
+
+	private void setLayouParams(Button button, boolean marginToLeft) {
+		int size = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48, mContext.getResources().getDisplayMetrics());
+
+		LinearLayout.LayoutParams btnParams = new LinearLayout.LayoutParams(size, size);
+		btnParams.gravity = Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL;
+		int margin = (int)
+				TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+						8,
+						mContext.getResources().getDisplayMetrics());
+
+		if(marginToLeft)
+			btnParams.setMarginStart(margin);
+		else
+			btnParams.setMarginEnd(margin);
+
+		button.setLayoutParams(btnParams);
 	}
 
 	public void expandAll(boolean expand) {
@@ -185,7 +235,6 @@ public class NotificationExpander extends XposedModPack {
 				setRowExpansion(row, expand);
 			}
 		}
-
 	}
 
 	private void setRowExpansion(Object row, boolean expand) {
