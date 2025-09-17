a78410cbe21730b63e4ca1ca9ecdcff15a0602f1
GitHub <PixelXpert@siava.sh>
2025-09-17 08:00:47 +0000

    Version update: Release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
 		applicationId = "sh.siava.pixelxpert"
 		minSdk = 36
 		targetSdk = 36
-		versionCode = 467
-		versionName = "canary-467"
+		versionCode = 468
+		versionName = "canary-468"
 		setProperty("archivesBaseName", "PixelXpert.apk")
 		ndk {
 			//noinspection ChromeOsAbiSupport
