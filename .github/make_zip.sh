96cbfe1af64d1014ce5c182ed08f33c272b0cab7
Siavash <25217714+siavash79@users.noreply.github.com>
2026-04-15 10:44:18 +0330

    part 2 - migration of magisk module build to gradle script

--- .github/make_zip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p output
-cp app/build/outputs/apk/release/PixelXpert.apk MagiskModBase/system/priv-app/PixelXpert/PixelXpert.apk
-cd MagiskModBase;
-FILENAME="PixelXpert.zip"
-
-echo 1 > build.type
-zip -r ../output/$FILENAME *;
